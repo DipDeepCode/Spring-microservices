533d4b4d6eaa028db0c6d74e9497936bfd208741
Sergey Kovalev <dip.deep@mail.ru>
2023-10-31 18:24:20 +0300

    добавлена возможность запуска нескольких инстансов eureka-client

--- eureka-client/src/main/java/ru/ddc/microdemo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/ddc/microdemo/eurekaclient/controller/TestController.java
@@ -1,13 +1,17 @@
 package ru.ddc.microdemo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "client instance id = " + id;
     }
 }
